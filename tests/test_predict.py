6b954a1703ff067f7fcb60e1a9bfe89e2fab86dd
Noa Margeta <nmargeta@gmail.com>
2024-01-12 19:19:07 +0100

    Add testing dataset to pytest test

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -17,7 +17,7 @@ def test_load_attribute_names():
 @pytest.mark.parametrize("image", ["image_0.jpg", "image_5.jpg", "image_17.jpg"])
 @pytest.mark.parametrize("model_path", [os.path.join(_PATH_MODEL, "model0")])
 def test_transform_image(image, model_path):
-    image_path = os.path.join("data/processed/images", image)
+    image_path = os.path.join("data/testing/images", image)
     processor = ViTImageProcessor.from_pretrained(model_path)
     transformed = transform_image(image_path, processor)
     assert type(transformed) == BatchFeature
@@ -26,7 +26,7 @@ def test_transform_image(image, model_path):
 @pytest.mark.parametrize("image", ["image_1.jpg", "image_3.jpg", "image_9.jpg"])
 @pytest.mark.parametrize("model_path", [os.path.join(_PATH_MODEL, "model0")])
 def test_predict(image, model_path):
-    image_path = os.path.join("data/processed/images", image)
+    image_path = os.path.join("data/testing/images", image)
     processor = ViTImageProcessor.from_pretrained(model_path)
     model = ViTForImageClassification.from_pretrained(model_path)
     model.eval()
