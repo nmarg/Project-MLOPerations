885a5626c160ff7e31bb31935a4739c02109fe07
Filip Penzar <fpenzar@gmail.com>
2024-01-13 18:24:23 +0100

    Change model to one-class problem

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,17 +1,9 @@
 import pytest
-from src.predict_model import load_attribute_names, transform_image, predict
+from src.predict_model import transform_image, predict
 import os
 from transformers import ViTImageProcessor, ViTForImageClassification
 from transformers.image_processing_utils import BatchFeature
 from tests import _PATH_MODEL
-
-
-def test_load_attribute_names():
-    att_names = load_attribute_names()
-    assert len(att_names) == 40
-    atts_to_check = ["Arched_Eyebrows", "Big_Nose", "High_Cheekbones", "Sideburns"]
-    for att in atts_to_check:
-        assert att in att_names
 
 
 @pytest.mark.parametrize("image", ["image_0.jpg", "image_5.jpg", "image_17.jpg"])
@@ -31,8 +23,7 @@ def test_predict(image, model_path):
     model = ViTForImageClassification.from_pretrained(model_path)
     model.eval()
 
-    att_names = load_attribute_names()
     image = transform_image(image_path, processor)
 
-    atts = predict(model, image, att_names)
-    assert isinstance(atts, list)
+    atts = predict(model, image)
+    assert isinstance(atts, str)
