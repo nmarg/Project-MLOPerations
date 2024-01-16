53d614f0cbf740543d18f6c6e38fa84cf0305c37
Filip Penzar <fpenzar@gmail.com>
2024-01-16 12:52:31 +0100

    Revert "Reformat __init__"
    This reverts commit 39321591d4a5c03ccfd3d7adeb81d659d9cb7bf1.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,3 @@
-from .models import *
-from .data import *
-from .server import *
-
-from .predict_model import transform_image, predict
-from .train_model import train
+from src.data.make_dataset import CelebADataModule, CustomImageDataset
+from src.predict_model import predict
+from src.server.main import app
--- src/data/__init__.py
+++
@@ -1,1 +0,0 @@
-from .make_dataset import CelebADataModule, CustomImageDataset
