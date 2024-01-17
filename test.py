bce6adaddf05692afe48dd0daaa661be24bc1fd3
Filip Penzar <fpenzar@gmail.com>
2024-01-17 20:14:21 +0100

    Test gcs upload

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+from google.cloud import storage
+
+
+def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+
+upload_to_gcs("project-mloperations-data", "data/drifting/current_data.csv", "data/drifting/current_data.csv")
