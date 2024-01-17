9ce7e4bfd93ea39551d06ac82ef6e4d1e591be4d
Filip Penzar <fpenzar@gmail.com>
2024-01-17 11:16:57 +0100

    Fix imports

--- src/server/main.py
+++ src/server/main.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from io import BytesIO
 from csv import writer
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks
-from fastapi.response import HTMLResponse
+from fastapi.responses import HTMLResponse
 from PIL import Image
 from transformers import ViTForImageClassification, ViTImageProcessor
 from datetime import datetime
@@ -21,6 +21,7 @@ model = ViTForImageClassification.from_pretrained(model_path)
 model.eval()
 processor = ViTImageProcessor.from_pretrained(model_path)
 
+
 def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     storage_client = storage.Client()
@@ -28,6 +29,7 @@ def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
+
 
 def save_image_prediction(image, inference):
     image_params = calculate_image_params(image)
@@ -40,7 +42,7 @@ def save_image_prediction(image, inference):
 
     csv_row.append(inference)
 
-    with open('data/drifting/current_data.csv', 'a') as f_object:
+    with open("data/drifting/current_data.csv", "a") as f_object:
         now = datetime.now()
 
         csv_row.append(now.strftime("%m/%d/%Y, %H:%M:%S"))
@@ -48,10 +50,10 @@ def save_image_prediction(image, inference):
         writer_object = writer(f_object)
 
         writer_object.writerow(csv_row)
-    
+
         f_object.close()
-    
-    upload_to_gcs('project-mloperations-data', 'data/drifting/current_data.csv', 'data/drifting/current_data.csv')
+
+    upload_to_gcs("project-mloperations-data", "data/drifting/current_data.csv", "data/drifting/current_data.csv")
 
 
 @app.post("/predict/")
@@ -82,20 +84,20 @@ async def server_predict(background_tasks: BackgroundTasks, data: UploadFile = F
 
     background_tasks.add_task(save_image_prediction, image, inference)
 
-
     return response
+
 
 @app.get("/data-drifting-report", response_class=HTMLResponse)
 def data_drifting_report():
-    reference_data = pd.read_csv('gs://project-mloperations-data/drifting/reference_data.csv')
-    current_data = pd.read_csv('gs://project-mloperations-data/drifting/current_data.csv')
+    reference_data = pd.read_csv("gs://project-mloperations-data/drifting/reference_data.csv")
+    current_data = pd.read_csv("gs://project-mloperations-data/drifting/current_data.csv")
 
     last_column_name = current_data.columns[-1]
     current_data = current_data.drop(last_column_name, axis=1)
 
     report = Report(metrics=[DataDriftPreset(), DataQualityPreset()])
     report.run(reference_data=reference_data, current_data=current_data)
-    report.save_html('report.html')
+    report.save_html("report.html")
 
     with open("report.html", "r") as file:
         html_content = file.read()
