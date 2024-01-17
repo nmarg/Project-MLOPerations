eb3bdf271169f1dd8894fe0ba6c399c01071ddaf
Ana Marija <ana-marija.pavicic@fer.hr>
2024-01-17 17:38:35 +0100

    Change tests to True

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -3,35 +3,33 @@ from src.train_model import train
 from omegaconf import OmegaConf
 import shutil
 
+
 def test_model_training():
-   # Test the training process with a small dummy dataset
-   dummy_config = {
-      "pretrained_model_path" : "google/vit-base-patch16-224",
-      "num_labels": 1,
-      "batch_size": 64,
-      "metric": "macro",
-      "output_dir" : "./test_training_outputs",
-      "epochs": 1,
-      "lr": 0.01,
-      "model_output_dir": "./test_models",
-      "reproducible_experiment": True,
-      "seed": 15,
-      "light_weight": True,
-      "test" : False,
-   }
-   config = OmegaConf.create(dummy_config)
-   
-   train(config)
+    # Test the training process with a small dummy dataset
+    dummy_config = {
+        "pretrained_model_path": "google/vit-base-patch16-224",
+        "num_labels": 1,
+        "batch_size": 64,
+        "metric": "macro",
+        "output_dir": "./test_training_outputs",
+        "epochs": 1,
+        "lr": 0.01,
+        "model_output_dir": "./test_models",
+        "reproducible_experiment": True,
+        "seed": 15,
+        "light_weight": True,
+        "test": True,
+    }
+    config = OmegaConf.create(dummy_config)
 
-   # Assert if model is saved and metrics are logged
-   assert os.path.exists(dummy_config["output_dir"])
-   assert os.path.exists(dummy_config["model_output_dir"])
+    train(config)
 
-   # Cleanup: Remove the created directories
-   if os.path.exists(dummy_config["output_dir"]):
-      shutil.rmtree(dummy_config["output_dir"])
-   if os.path.exists(dummy_config["model_output_dir"]):
-      shutil.rmtree(dummy_config["model_output_dir"])
-      
+    # Assert if model is saved and metrics are logged
+    assert os.path.exists(dummy_config["output_dir"])
+    assert os.path.exists(dummy_config["model_output_dir"])
 
-
+    # Cleanup: Remove the created directories
+    if os.path.exists(dummy_config["output_dir"]):
+        shutil.rmtree(dummy_config["output_dir"])
+    if os.path.exists(dummy_config["model_output_dir"]):
+        shutil.rmtree(dummy_config["model_output_dir"])
