667436b363cc9dbc240b910200f73929ec2ddd7b
Jan Ljubas <86132019+janljubas@users.noreply.github.com>
2024-01-17 18:47:58 +0100

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,17 +1,14 @@
 import os
 
-import evaluate
-import hydra
 import torch
 from datasets import DatasetDict
+from models.model import make_model
 from transformers import Trainer, TrainingArguments, ViTImageProcessor, set_seed
-
 
 from data.make_dataset import CelebADataModule
 import hydra
 import evaluate
 import wandb
-
 
 _SRC_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_SRC_ROOT)
@@ -29,11 +26,8 @@ def collate_fn(batch):
     return data
 
 
-@hydra.main(
-    config_path=os.path.join(_PROJECT_ROOT, "config/model"),
-    config_name="model_config.yaml",
-    version_base=None,
-)
+@hydra.main(config_path=os.path.join(_PROJECT_ROOT, "config/model"), config_name="model_config.yaml", version_base=None)
+
 def train(cfg):
     """
     Train the model on processed data.
@@ -119,10 +113,6 @@ def train(cfg):
         tokenizer=processor,
     )
 
-    # if testing, don't save the results
-    if cfg.test:
-        return
-
     # train and save the model and metrics
     train_results = trainer.train()
     savedir = find_free_directory(cfg.model_output_dir)
@@ -151,5 +141,5 @@ def find_free_directory(savedir):
             return dir
 
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     train()
